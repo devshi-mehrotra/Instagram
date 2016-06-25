70fdcf57e94d05596d1f1297fa739c18a2f3a764
Devshi Mehrotra <devshim12@gmail.com>
2016-06-24 19:33:54 -0700

    UPDATE

---
+++ CommentCell.swift
@@ -0,0 +1,24 @@
+//
+//  CommentCell.swift
+//  Instagram
+//
+//  Created by Devshi Mehrotra on 6/24/16.
+//  Copyright © 2016 Devshi Mehrotra. All rights reserved.
+//
+
+import UIKit
+
+class CommentCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
